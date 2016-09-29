6d96cd07a8339467562f7143fe121ab74a4cd1c8
Romain Caire <romain.caire@doro.com>
2016-09-29 17:58:37 +0200

    Add HW Encryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,7 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
 
 # Filesystem
 BOARD_BOOTIMAGE_PARTITION_SIZE 		:= 0x04000000
